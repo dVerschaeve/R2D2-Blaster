a049f99501482e472c57f3f1b1ad64e093fe7f71
dverschaeve <dries.verschaeve@vddesign.be>
2020-06-20 12:38:58 +0200

    Initial Commit

---
+++ conf_sample.h
@@ -0,0 +1,25 @@
+//Rename to conf.h
+#define CONFIG_DEVICE_ID "NRSOffice"
+
+#define CONFIG_DHTPIN 5     // what digital pin we're connected to
+#define CONFIG_DHTTYPE DHT11   
+
+#define CONFIG_IRPIN 4
+
+// WiFi
+#define CONFIG_WIFI_SSID "SSID"
+#define CONFIG_WIFI_PASS "MySampleWifiPassword"
+
+// MQTT
+#define CONFIG_MQTT_HOST "mqtt.example.com"
+#define CONFIG_MQTT_PORT 1883
+#define CONFIG_MQTT_USER "MQTTUserName"
+#define CONFIG_MQTT_PASS "SamplePassword"
+#define CONFIG_MQTT_CLIENT_ID "R2D2Blaster" 
+
+// MQTT Topics
+#define CONFIG_MQTT_MODE_COMMAND_TOPIC "climate/R2D2/mode/set"
+#define CONFIG_MQTT_MODE_STATE_TOPIC "climate/R2D2/mode/state"
+#define CONFIG_MQTT_TEMPERATURE_COMMAND_TOPIC "climate/R2D2/temperature/set"
+#define CONFIG_MQTT_TEMPERATURE_STATE_TOPIC "climate/R2D2/temperature/state"
+#define CONFIG_MQTT_TEMPERATURE_SENSOR_TOPIC "climate/R2D2/temperature/sensor"
---
+++ nrsOffice.ino
@@ -0,0 +1,547 @@
+#include "conf.h"
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+#include "DHT.h"
+
+// WiFi
+WiFiClient espClient;
+
+// MQTT
+PubSubClient client(espClient);
+const int BUFFER_SIZE = JSON_OBJECT_SIZE(20);
+
+
+// DHT Sensor
+DHT dht(CONFIG_DHTPIN, CONFIG_DHTTYPE);
+long lastSensorMSG = 0;
+
+
+
+// General
+
+int halfPeriodicTime;
+int IRpin;
+int khz;
+
+typedef enum HvacMode {
+  HVAC_HOT,
+  HVAC_COLD,
+  HVAC_DRY,
+  HVAC_FAN, // used for Panasonic only
+  HVAC_AUTO
+} HvacMode_t; // HVAC  MODE
+
+typedef enum HvacFanMode {
+  FAN_SPEED_1,
+  FAN_SPEED_2,
+  FAN_SPEED_3,
+  FAN_SPEED_4,
+  FAN_SPEED_5,
+  FAN_SPEED_AUTO,
+  FAN_SPEED_SILENT
+} HvacFanMode_;  // HVAC  FAN MODE
+
+typedef enum HvacVanneMode {
+  VANNE_AUTO,
+  VANNE_H1,
+  VANNE_H2,
+  VANNE_H3,
+  VANNE_H4,
+  VANNE_H5,
+  VANNE_AUTO_MOVE
+} HvacVanneMode_;  // HVAC  VANNE MODE
+
+typedef enum HvacWideVanneMode {
+  WIDE_LEFT_END,
+  WIDE_LEFT,
+  WIDE_MIDDLE,
+  WIDE_RIGHT,
+  WIDE_RIGHT_END
+} HvacWideVanneMode_t;  // HVAC  WIDE VANNE MODE
+
+typedef enum HvacAreaMode {
+  AREA_SWING,
+  AREA_LEFT,
+  AREA_AUTO,
+  AREA_RIGHT
+} HvacAreaMode_t;  // HVAC  WIDE VANNE MODE
+
+typedef enum HvacProfileMode {
+  NORMAL,
+  QUIET,
+  BOOST
+} HvacProfileMode_t;  // HVAC PANASONIC OPTION MODE
+
+
+//Default Values
+HvacMode CURRENT_HVAC_Mode = HVAC_HOT;
+double CURRENT_TEMP = 21.0;
+bool HVAC_DISABLED = false;
+
+
+// HVAC MITSUBISHI_
+#define HVAC_MITSUBISHI_HDR_MARK    3400
+#define HVAC_MITSUBISHI_HDR_SPACE   1750
+#define HVAC_MITSUBISHI_BIT_MARK    450
+#define HVAC_MITSUBISHI_ONE_SPACE   1300
+#define HVAC_MISTUBISHI_ZERO_SPACE  420
+#define HVAC_MITSUBISHI_RPT_MARK    440
+#define HVAC_MITSUBISHI_RPT_SPACE   17100 // Above original iremote limit
+
+
+/****************************************************************************
+/* Send IR command to Mitsubishi HVAC - sendHvacMitsubishi
+/***************************************************************************/
+void sendHvacMitsubishi(
+  HvacMode                HVAC_Mode,           // Example HVAC_HOT  HvacMitsubishiMode
+  int                     HVAC_Temp,           // Example 21  (°c)
+  HvacFanMode             HVAC_FanMode,        // Example FAN_SPEED_AUTO  HvacMitsubishiFanMode
+  HvacVanneMode           HVAC_VanneMode,      // Example VANNE_AUTO_MOVE  HvacMitsubishiVanneMode
+  int                     OFF                  // Example false
+)
+{
+
+// #define  HVAC_MITSUBISHI_DEBUG;  // Un comment to access DEBUG information through Serial Interface
+
+  byte mask = 1; //our bitmask
+  byte data[18] = { 0x23, 0xCB, 0x26, 0x01, 0x00, 0x20, 0x08, 0x06, 0x30, 0x45, 0x67, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x1F };
+  // data array is a valid trame, only byte to be chnaged will be updated.
+
+  byte i;
+
+#ifdef HVAC_MITSUBISHI_DEBUG
+  Serial.println("Packet to send: ");
+  for (i = 0; i < 18; i++) {
+    Serial.print("_");
+    Serial.print(data[i], HEX);
+  }
+  Serial.println(".");
+#endif
+
+  // Byte 6 - On / Off
+  if (OFF) {
+    data[5] = (byte) 0x0; // Turn OFF HVAC
+  } else {
+    data[5] = (byte) 0x20; // Tuen ON HVAC
+  }
+
+  // Byte 7 - Mode
+  switch (HVAC_Mode)
+  {
+    case HVAC_HOT:   data[6] = (byte) 0x08; break;
+    case HVAC_COLD:  data[6] = (byte) 0x18; break;
+    case HVAC_DRY:   data[6] = (byte) 0x10; break;
+    case HVAC_AUTO:  data[6] = (byte) 0x20; break;
+    default: break;
+  }
+
+  // Byte 8 - Temperature
+  // Check Min Max For Hot Mode
+  byte Temp;
+  if (HVAC_Temp > 31) { Temp = 31;}
+  else if (HVAC_Temp < 16) { Temp = 16; } 
+  else { Temp = HVAC_Temp; };
+  data[7] = (byte) Temp - 16;
+
+  // Byte 10 - FAN / VANNE
+  switch (HVAC_FanMode)
+  {
+    case FAN_SPEED_1:       data[9] = (byte) B00000001; break;
+    case FAN_SPEED_2:       data[9] = (byte) B00000010; break;
+    case FAN_SPEED_3:       data[9] = (byte) B00000011; break;
+    case FAN_SPEED_4:       data[9] = (byte) B00000100; break;
+    case FAN_SPEED_5:       data[9] = (byte) B00000100; break; //No FAN speed 5 for MITSUBISHI so it is consider as Speed 4
+    case FAN_SPEED_AUTO:    data[9] = (byte) B10000000; break;
+    case FAN_SPEED_SILENT:  data[9] = (byte) B00000101; break;
+    default: break;
+  }
+
+  switch (HVAC_VanneMode)
+  {
+    case VANNE_AUTO:        data[9] = (byte) data[9] | B01000000; break;
+    case VANNE_H1:          data[9] = (byte) data[9] | B01001000; break;
+    case VANNE_H2:          data[9] = (byte) data[9] | B01010000; break;
+    case VANNE_H3:          data[9] = (byte) data[9] | B01011000; break;
+    case VANNE_H4:          data[9] = (byte) data[9] | B01100000; break;
+    case VANNE_H5:          data[9] = (byte) data[9] | B01101000; break;
+    case VANNE_AUTO_MOVE:   data[9] = (byte) data[9] | B01111000; break;
+    default: break;
+  }
+
+  // Byte 18 - CRC
+  data[17] = 0;
+  for (i = 0; i < 17; i++) {
+    data[17] = (byte) data[i] + data[17];  // CRC is a simple bits addition
+  }
+
+#ifdef HVAC_MITSUBISHI_DEBUG
+  Serial.println("Packet to send: ");
+  for (i = 0; i < 18; i++) {
+    Serial.print("_"); Serial.print(data[i], HEX);
+  }
+  Serial.println(".");
+  for (i = 0; i < 18; i++) {
+    Serial.print(data[i], BIN); Serial.print(" ");
+  }
+  Serial.println(".");
+#endif
+
+  enableIROut(38);  // 38khz
+  space(0);
+  for (int j = 0; j < 2; j++) {  // For Mitsubishi IR protocol we have to send two time the packet data
+    // Header for the Packet
+    mark(HVAC_MITSUBISHI_HDR_MARK);
+    space(HVAC_MITSUBISHI_HDR_SPACE);
+    for (i = 0; i < 18; i++) {
+      // Send all Bits from Byte Data in Reverse Order
+      for (mask = 00000001; mask > 0; mask <<= 1) { //iterate through bit mask
+        if (data[i] & mask) { // Bit ONE
+          mark(HVAC_MITSUBISHI_BIT_MARK);
+          space(HVAC_MITSUBISHI_ONE_SPACE);
+        }
+        else { // Bit ZERO
+          mark(HVAC_MITSUBISHI_BIT_MARK);
+          space(HVAC_MISTUBISHI_ZERO_SPACE);
+        }
+        //Next bits
+      }
+    }
+    // End of Packet and retransmission of the Packet
+    mark(HVAC_MITSUBISHI_RPT_MARK);
+    space(HVAC_MITSUBISHI_RPT_SPACE);
+    space(0); // Just to be sure
+  }
+}
+
+/****************************************************************************
+/* enableIROut : Set global Variable for Frequency IR Emission
+/***************************************************************************/ 
+void enableIROut(int khz) {
+  // Enables IR output.  The khz value controls the modulation frequency in kilohertz.
+  halfPeriodicTime = 500/khz; // T = 1/f but we need T/2 in microsecond and f is in kHz
+}
+
+/****************************************************************************
+/* mark ( int time) 
+/***************************************************************************/ 
+void mark(int time) {
+  // Sends an IR mark for the specified number of microseconds.
+  // The mark output is modulated at the PWM frequency.
+  long beginning = micros();
+  while(micros() - beginning < time){
+    digitalWrite(IRpin, HIGH);
+    delayMicroseconds(halfPeriodicTime);
+    digitalWrite(IRpin, LOW);
+    delayMicroseconds(halfPeriodicTime); //38 kHz -> T = 26.31 microsec (periodic time), half of it is 13
+  }
+}
+
+/****************************************************************************
+/* space ( int time) 
+/***************************************************************************/ 
+/* Leave pin off for time (given in microseconds) */
+void space(int time) {
+  // Sends an IR space for the specified number of microseconds.
+  // A space is no output, so the PWM output is disabled.
+  digitalWrite(IRpin, LOW);
+  if (time > 0) delayMicroseconds(time);
+}
+
+/****************************************************************************
+/* sendRaw (unsigned int buf[], int len, int hz)
+/***************************************************************************/ 
+void sendRaw (unsigned int buf[], int len, int hz)
+{
+  enableIROut(hz);
+  for (int i = 0; i < len; i++) {
+    if (i & 1) {
+      space(buf[i]);
+    } 
+    else {
+      mark(buf[i]);
+    }
+  }
+  space(0); // Just to be sure
+}
+
+
+/****************************************************************************
+/* readDHT read and Print DHT Values
+/***************************************************************************/
+void readDHT() {
+  delay(2000);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.print(" *C ");
+  Serial.print(f);
+  Serial.print(" *F\t");
+  Serial.print("Heat index: ");
+  Serial.print(hic);
+  Serial.print(" *C ");
+  Serial.print(hif);
+  Serial.println(" *F");
+
+  long now = millis();
+
+  if (now - lastSensorMSG  > 60000) {
+    lastSensorMSG = now;
+    Serial.print("Publish sensor data");
+    //String temperature;
+    //temperature = String(t);
+    //client.publish(CONFIG_MQTT_TEMPERATURE_SENSOR_TOPIC, (char*) temperature.c_str(), true);
+    
+    StaticJsonBuffer<BUFFER_SIZE> jsonBuffer;
+    JsonObject& root = jsonBuffer.createObject();
+    root["Temperature"] = t;
+    root["Humidity"] = h;
+    root["HeatIndex"] = hic;
+    char buffer[root.measureLength() + 1];
+    root.printTo(buffer, sizeof(buffer));
+
+    client.publish(CONFIG_MQTT_TEMPERATURE_SENSOR_TOPIC, buffer, true);
+  }
+}
+
+
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(CONFIG_WIFI_SSID);
+
+  WiFi.mode(WIFI_STA); // Disable the built-in WiFi access point.
+  WiFi.begin(CONFIG_WIFI_SSID, CONFIG_WIFI_PASS);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+
+  ArduinoOTA.setHostname(CONFIG_DEVICE_ID);
+  ArduinoOTA.begin();
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+
+  char message[length + 1];
+  for (int i = 0; i < length; i++) {
+    message[i] = (char)payload[i];
+  }
+  message[length] = '\0';
+  Serial.println(message);
+
+  String strTopic = String(topic);
+
+
+  if(String(topic) == CONFIG_MQTT_MODE_COMMAND_TOPIC){
+    Serial.println("New Mode command received");
+
+    if(String(message) == "heat"){
+      HVAC_DISABLED = false;
+      CURRENT_HVAC_Mode = HVAC_HOT;
+    }
+
+    if(String(message) == "cool"){
+      HVAC_DISABLED = false;
+      CURRENT_HVAC_Mode = HVAC_COLD;
+    }
+
+    if(String(message) == "off"){
+      HVAC_DISABLED = true;
+    }
+
+    sendHvacMitsubishi(CURRENT_HVAC_Mode, CURRENT_TEMP, FAN_SPEED_AUTO, VANNE_AUTO_MOVE, HVAC_DISABLED);
+    client.publish(CONFIG_MQTT_MODE_STATE_TOPIC, message, true);
+    
+    String t;
+    t = String(CURRENT_TEMP);
+    client.publish(CONFIG_MQTT_TEMPERATURE_STATE_TOPIC,(char*) t.c_str(), true);
+  
+  }
+
+  if(String(topic) == CONFIG_MQTT_TEMPERATURE_COMMAND_TOPIC){
+    Serial.println("New temperature command received");
+
+    String strTemp = String(message);
+    float float_var = strTemp.toFloat();
+    CURRENT_TEMP = float_var;
+    
+    String t;
+    t = String(CURRENT_TEMP);
+    
+    sendHvacMitsubishi(CURRENT_HVAC_Mode, CURRENT_TEMP, FAN_SPEED_AUTO, VANNE_AUTO_MOVE, false);
+    client.publish(CONFIG_MQTT_TEMPERATURE_STATE_TOPIC,(char*) t.c_str(), true);
+
+    String strMode;
+    switch (CURRENT_HVAC_Mode)
+    {
+      case HVAC_HOT:  strMode = "heat"; break;
+      case HVAC_COLD: strMode = "cool"; break;
+      case HVAC_DRY:  strMode = "dry"; break;
+      case HVAC_AUTO: strMode = "auto"; break;
+      default: break;
+    }
+
+    //String strMode = "heat";
+    client.publish(CONFIG_MQTT_MODE_STATE_TOPIC, (char*) strMode.c_str(), true);
+    
+  }
+  
+    //if (!processJson(message)) {
+    //  return;
+    //}
+    //Serial.println("Sending state to MQTT Server");
+    //sendState();
+
+}
+
+bool processJson(char* message){
+  Serial.println("Processing JSON");
+  StaticJsonBuffer<BUFFER_SIZE> jsonBuffer;
+  JsonObject& root = jsonBuffer.parseObject(message);
+
+  if (!root.success()) {
+    Serial.println("parseObject() failed");
+    return false;
+  }
+
+   root.printTo(Serial);
+
+ 
+  
+  return true;
+}
+
+
+void sendState() {
+  Serial.print("Sending State:");
+
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect(CONFIG_DEVICE_ID, CONFIG_MQTT_USER, CONFIG_MQTT_PASS)) {
+      Serial.println("connected");
+      client.subscribe(CONFIG_MQTT_MODE_COMMAND_TOPIC);
+      client.subscribe(CONFIG_MQTT_TEMPERATURE_COMMAND_TOPIC);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+
+
+
+
+/****************************************************************************
+/* setup ()
+*  Note: That's also the entry point after DeepSleep TimeOut 
+*/
+/***************************************************************************/
+void setup() {
+
+  Serial.begin(115200);
+  Serial.println("Board Started");
+  
+  delay(3000); // sanity delay
+  
+  //WiFi Setup
+  setup_wifi();
+
+  //MQTT Setup
+  client.setServer(CONFIG_MQTT_HOST, CONFIG_MQTT_PORT);
+  client.setCallback(callback);
+  
+  
+  // put your setup code here, to run once:
+  IRpin=CONFIG_IRPIN;
+  khz=38;
+  halfPeriodicTime = 500/khz;
+  pinMode(IRpin, OUTPUT);
+
+}
+
+/****************************************************************************
+/* Loop ()
+/***************************************************************************/
+void loop() {
+      
+      //MQTT Client Connection
+      if (!client.connected()) {
+        Serial.println("not connected");
+        reconnect();
+      }
+      
+      client.loop();
+      readDHT();
+
+      ArduinoOTA.handle();
+}
